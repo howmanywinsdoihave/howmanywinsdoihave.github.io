4961338b2c8252e25bfd24c1821f092275f3ba69
james <37032110+howmanywinsdoihave@users.noreply.github.com>
2020-04-18 15:13:58 +1000

    v2

--- game.js
+++ game.js
@@ -1,7 +1,12 @@
+function getNoOfWins() {
+var nameOfPlayer = document.getElementById("playersName").value
+            var ele = document.getElementsByName('platformId');            
+            for(i = 0; i < ele.length; i++) { 
+                if(ele[i].checked) 
+                var platform = ele[i].value; 
+            } 
 const http1 = new XMLHttpRequest();
-const url = 'https://cors-anywhere.herokuapp.com/https://api.fortnitetracker.com/v1/profile/pc/sailin.';
-
-
+const url = 'https://cors-anywhere.herokuapp.com/https://api.fortnitetracker.com/v1/profile/'+ platform + '/' + nameOfPlayer;
 http1.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     var stats = JSON.parse(this.responseText);
@@ -9,7 +14,7 @@ http1.onreadystatechange = function() {
     var noOfWins = JSON.stringify(stats.lifeTimeStats[8])
     var n = noOfWins.length
     noOfWins = noOfWins.slice(23,n-2)
-    document.getElementById("statsPage").innerHTML = noOfWins;
+    document.getElementById("statsPage").innerHTML = "You have " + noOfWins + " wins";
   }
 };
 http1.open("GET", url, true);
@@ -17,3 +22,4 @@ http1.setRequestHeader("TRN-Api-Key", "b1e03cea-8652-40f1-8300-1239bbb0a908", tr
 http1.setRequestHeader("Accept", "application/json", true);
 
 http1.send();
+}
